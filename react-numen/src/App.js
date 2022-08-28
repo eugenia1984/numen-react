feab37d6fc23728d3114eae2f51356b7b96d604e
Julieta <julieta@MacBook-Pro-de-Alejandro.local>
2022-08-28 20:25:04 -0300

    arme codigo base del AboutUs, no me levanta el proyecto. Falta hablar con el profe para ver de armar ls BD den el json server

--- react-numen/src/App.js
+++ react-numen/src/App.js
@@ -4,8 +4,47 @@ import { Header } from "./components/header/Header";
 import { Footer } from "./components/footer/Footer";
 import { Products } from "./pages/Products";
 import { Warning } from "./components/warning/Warning";
+/*import imagenEuge from "./components/aboutus/imgs/euge.jpg";
+import imagenEkel from "./components/aboutus/imgs/ekel.jpg";
+import imagenTincho from "./components/aboutus/imgs/tincho.jpg";
+import imagenJuli from "./components/aboutus/imgs/juli.jpg";*/
+import { AboutUs } from "./components/aboutus/AboutUs";
+
 
 function App() {
+
+const aboutUsEuge = {
+  id:1,
+  nombre: `Maria Eugenia`,
+  Ocupacion: `Team Leader`,
+  /*imagen: imagenEuge,*/
+}
+  
+
+const aboutUsEkel = {
+  id:2,
+  nombre: `Ekel Leal`,
+  Ocupacion: `FrontEnd Developer`,
+  /*imagen: imagenEkel,*/
+
+}
+
+const aboutUsTincho = {
+  id:3,
+  nombre: `Martín La Rosa`,
+  Ocupacion: `BackEnd Developer`,
+  /*imagen: imagenTincho,*/
+
+}
+
+const aboutUsJuli = {
+  id:4,
+  nombre: `Julieta Marcer`,
+  Ocupacion: `UX/UI Designer`,
+  /*imagen: imagenJuli,*/
+
+}
+
   return (
     <>
     <Warning/>
@@ -14,6 +53,13 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
       </Routes>
+
+      <div style={{display:`grid` , width: `900px`, justifyContent: `space-between`, gap :10, flexDirection: `row`, gridTemplateColumns: `repeat (4, 1fr`}}>
+      <AboutUs aboutUs={aboutUsEuge}/>
+      <AboutUs aboutUs={aboutUsEkel}/>     
+      <AboutUs aboutUs={aboutUsTincho}/>
+      <AboutUs aboutUs={aboutUsJuli}/>
+      </div>
       <Footer />
     </>
   );
--- react-numen/src/components/aboutus/AboutUs.jsx
+++ react-numen/src/components/aboutus/AboutUs.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+/*import `/AboutUs.css`;*/
 
-const AboutUs = () => {
+const AboutUs = (props) => {
+  
   return (
-    <div>AboutUs</div>
+    <div className="AboutUsCss" style={{color: blue,
+      width: 200,
+      display: `block`,
+      textAlign: `center` ,
+      borderRadius: 8,
+      border:`2px solid rgba(30, 30, 30)`,}}>
+
+      <img src={props.aboutUs.imagen} alt="img" style={{height: 200, maxWidth: `100%`}}/>
+
+      <h2>{props.aboutUs.nombre} </h2>
+
+      <p>{props.aboutUs.ocupacion}</p>
+
+    </div>
   )
 }
 
