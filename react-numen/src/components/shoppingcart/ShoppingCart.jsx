9bc0b626f18003610f128de0011074178ec306ef
TinchoLarrosa <martinnicolas.larrosa@gmail.com>
2022-09-19 22:23:46 -0300

    creo proyecto nuevamente

--- react-numen/src/components/shoppingcart/ShoppingCart.jsx
+++ react-numen/src/components/shoppingcart/ShoppingCart.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const ShoppingCart = () => {
+
+  const [state, dispatch] = useReducer(shoppingReducer, shoppingInitialState)
+
+  const addToCart = () => { };
+
+  const deleteFromCart = () => { };
+
+  const clearCart = () => { };
+
+
   return (
-    <div>ShoppingCart</div>
+    <div>
+      <h2>Shopping Cart</h2>
+      <h3>Products</h3>
+      <div className="shopping_cart"></div>
+      <h3>Carrito</h3>
+      <div className="box"></div>
+      <button>Limpiar Carrito</button>
+    </div>
   )
 }
 
-export {ShoppingCart};
+export { ShoppingCart };
---
+++ react-numen/src/reducers/shoppingReducers.js
@@ -0,0 +1,9 @@
+export const shoppingInitialState ={
+    products: [
+        {id:1,name:"Pagina Web", price: 2000},
+        {id:2,name:"App Android", price: 1500},
+        {id:3,name:"App Ios", price: 1500},
+        {id:4,name:"App Android & Ios", price: 2800},
+        {id:5,name:"Pagina Web & App Movile", price:4500}
+    ]
+}
