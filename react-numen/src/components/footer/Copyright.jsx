920bd3df9cffb7ccc9d257e3a5ed3746e52fc6d6
eugenia1984 <mariaeugeniacosta1@gmail.com>
2022-09-28 20:40:25 -0300

    agrego el footer

---
+++ react-numen/src/components/footer/Copyright.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+import "./Footer.css";
+
+const Copyright = () => {
+  return (
+    <div>
+      <p className="main-footer-copyright">Copywrite 2022 - Todos los derechos reservados</p>
+    </div>
+  );
+};
+
+export default Copyright;
